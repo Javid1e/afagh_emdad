0c4978138c1b1e9f027eef4e14b08606d61545ab
Abdollah Mohebbi <Abdollah.promail@gmail.com>
2024-06-06 02:38:08 +0330

    Add  Mutations.graphql & Queries.graphql & schema.graphql & commands/graphql_schema.py

--- app/web/web/profiles/validations.py
+++ app/web/web/profiles/validations.py
@@ -1,9 +1,11 @@
 # profiles/validations.py
 from django.core.exceptions import ValidationError
 
+
 def validate_personal_information(value):
     if not isinstance(value, dict):
         raise ValidationError('Personal information must be a JSON object.')
+
 
 def validate_car_information(value):
     if not isinstance(value, dict):
